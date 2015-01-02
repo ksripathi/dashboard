f35a164e5c5ca3efe1fae3229b10ab9e5d5a2760
sripathi <kammari.sripathi@gmail.com>
2015-01-02 17:01:35 +0530

    finished till developers tab

--- functions.js
+++ functions.js
@@ -1,4 +1,30 @@
+function showDevelopers(str)
+{
 
+    if (str == "")
+    {
+        document.getElementById("developers").innerHTML = "";
+        return;
+    } 
+    else 
+    {
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                document.getElementById("developers").innerHTML = xmlhttp.responseText;
+            }
+        }
+        xmlhttp.open("GET","getdevelopers.php",true);
+        xmlhttp.send();
+    }
+
+}
 function showLabs(str) 
 {
     if (str == "")
