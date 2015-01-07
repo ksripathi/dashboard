774404156635ed61d5ff6a73a662690b687b9212
sripathi <kammari.sripathi@gmail.com>
2015-01-07 12:03:01 +0530

    added phase tab

--- functions.js
+++ functions.js
@@ -1,3 +1,31 @@
+function showPhase(str)
+{
+
+    if (str == "")
+    {
+        document.getElementById("phase").innerHTML = "";
+        return;
+    } 
+    else 
+    {
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                document.getElementById("phase").innerHTML = xmlhttp.responseText;
+            }
+        }
+        xmlhttp.open("GET","getcombos.php",true);
+        xmlhttp.send();
+    }
+
+}
+
 function showTechnologies(str)
 {
 
